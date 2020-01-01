78a3c8219c8ac705305b260f40777be69dc54312
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-01-02 00:35:14 +0530

    scraper for CES Exhibitors addeD

--- ces_ex.py
+++ ces_ex.py
@@ -6,7 +6,7 @@ import re
 data = []
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
-url = "/Users/vivekkaushal/Downloads/file.htm"
+url = "/Users/vivekkaushal/Desktop/index.htm"
 try:
   # r = requests.get(url, headers=headers, timeout=5)
   page = open(url)
@@ -14,20 +14,23 @@ except:
   print('Could not reach website')
   quit()
 
+data =[]
+
 soup = BeautifulSoup(page, 'html.parser')
-articles = soup.find("article", {"id": "speaker-listing"})
-aside = articles.find_all("aside")
-for i in aside:
+# articles = soup.find("article", {"id": "speaker-listing"})
+company = soup.find_all("div",{"class": "exhibitorRow listingCard p-2 bd-highlight"})
+for i in company:
+  name = i.find('a').contents[0]
   link = i.find('a')['href']
-  name = i.find('h3').text
-  details = i.find_all('h4')
-  points = {
-    "link": str(link),
-    "name":name,
-    "company":details[1].text,
-    "position":details[0].text
+  footer = i.find('footer')
+  booth = footer.find('a').contents[0]
+  data_dict = {
+    'name':name,
+    'link':link,
+    'booth':booth
   }
-  data.append(points)
+  data.append(data_dict)
+
 
 keys = data[0].keys()
 with open('data.csv', 'w+') as output_file:
